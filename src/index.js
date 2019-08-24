7601cea8d06259bd991d7936b20fe6ef1b2c5289
kesslern <nathant93@gmail.com>
2019-08-24 17:59:51 -0400

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from "react-dom";
+import { TextInput } from "./lib";
+import { useLoadState, LoadStateManager } from './lib/LoadStateContext';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  const test1 = useLoadState('test1')
+  const test2 = useLoadState('test2')
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+  return (
+    <div style={{ width: 640, margin: "15px auto" }}>
+
+      <h1>Hello React</h1>
+      <button onClick={test1.toggle}>Click</button>
+      <button onClick={test2.toggle}>Click</button>
+      <TextInput label="Email Address" placeholder="name@example.com" />
+    </div>
+  )
+}
+
+render(<LoadStateManager><App /></LoadStateManager>, document.getElementById("root"));
